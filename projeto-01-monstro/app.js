c45033da6840059516925a593d85aa171d48f338
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-02-07 23:02:12 -0300

    Implementando o painel de pontuações e exibindo o resultado

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,15 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-
+        playerLife: 0,
+        monsterLife: 20,
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
 
     },
     watch: {
-        
+
     }
 })
