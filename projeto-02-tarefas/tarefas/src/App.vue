51bcce6a5f27220bfd3435195b5a390e877dc402
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-03-17 23:35:00 -0300

    adicionando o evento, ao clicar no elemento tarefa, modificar seu status

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Tarefas</h1>
 		<NewTask @taskAdded="addTask" />
-		<TaskGrid :tasks="tasks" @taskDeleted="deleteTask" />
+		<TaskGrid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChanged="toggleTaskState" />
 	</div>
 </template>
 
@@ -34,6 +34,9 @@ export default {
 		},
 		deleteTask(i) {
 			this.tasks.splice(i, 1);
+		},
+		toggleTaskState(i) {
+			this.tasks[i].pending = !this.tasks[i].pending;
 		}
 	},
 }
