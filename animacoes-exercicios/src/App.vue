12f97098cf4165ce6887f735157da5906975fad4
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-06-01 21:45:48 -0300

    Adicionando animação do tipo animation e transition

---
+++ animacoes-exercicios/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar mensagem</b-button>
+
+		<transition name="fade">
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+		<!-- TYPE é utilizado para sinalizadar o tipo da animação mandatoria, 
+		quando existe mais de um tipo e o tempo de animação é diferente -->
+		<!-- APPEAR é utilizado para que a animação aconteça no carregamento do componente -->
+		<transition name="slide" type="animation" appear>
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			msg: 'Uma mensagem de informação para o usuário',
+			exibir: true
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 2s;
+}
+/*
+.fade-enter-to {
+	opacity: 1;
+}
+
+.fade-leave {
+	opacity: 1;
+}
+
+.fade-leave-active {
+	transition: opacity 2s;
+}
+
+.fade-leave-to {
+	opacity: 0;
+}*/
+
+@keyframes slide-in {
+	from { transform: translateY(40px); }
+	to { transform: translateY(0); }
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0); }
+	to { transform: translateY(40px); }
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-leave-active {
+	animation: slide-out 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-enter, .slide-leave-to {
+	opacity: 0;
+}
+
+</style>
