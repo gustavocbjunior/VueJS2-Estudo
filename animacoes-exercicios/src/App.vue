870bbb21ea62889fa78026f326de25194a6e1e36
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-06-01 21:50:19 -0300

    Utilizando a biblioteca animate.css

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -13,6 +13,12 @@
 		<transition name="slide" type="animation" appear>
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
+
+		<transition 
+			enter-active-class="animated bounce"
+			leave-active-class="animated shake">
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
 	</div>
 </template>
 
@@ -22,7 +28,7 @@ export default {
 	data() {
 		return {
 			msg: 'Uma mensagem de informação para o usuário',
-			exibir: true
+			exibir: false
 		}
 	}
 }
