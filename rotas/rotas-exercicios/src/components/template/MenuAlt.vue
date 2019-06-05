57a0f2311d353c8cacc3954bd6036b823b4047b8
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-06-04 22:47:58 -0300

    Multiplos router views (router views nomeados)

---
+++ rotas/rotas-exercicios/src/components/template/MenuAlt.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <!-- exact define que a rota é exatamente barra, não ficando ativo com /usuario -->
+            <router-link to="/" tag="li" active-class="active" exact>
+                <a>Home</a>
+            </router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                <a>User</a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
