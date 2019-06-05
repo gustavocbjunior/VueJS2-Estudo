4b10e1a560f39ba17e1c45d5d58b62a2da7cb68e
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-06-04 21:37:25 -0300

    Modificando o modo de rota para history e criando o menu com router link

---
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <!-- exact define que a rota é exatamente barra, não ficando ativo com /usuario -->
+            <router-link to="/" tag="li" active-class="active" exact>
+                <a>Início</a>
+            </router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                <a>Usuário</a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
