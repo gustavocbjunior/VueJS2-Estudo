760c9d9a888da4fe638f216f944cd6c7440c84d6
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-09-26 22:39:40 -0300

    Adicionando validações nos botões de compra e venda

--- projeto-03-acoes/stock-trader/src/components/portfolio/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/portfolio/Stock.vue
@@ -10,10 +10,12 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
+                <v-text-field label="Quantidade" type="number" 
+                    :error="quantity < 0 || !Number.isInteger(quantity) || insufficientQuantity"
+                    v-model.number="quantity"></v-text-field>
                 <v-btn class="blue darken-3 white--text" 
-                    :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    @click="sellStock">Vender</v-btn>
+                    :disabled="quantity <= 0 || !Number.isInteger(quantity) || insufficientQuantity"
+                    @click="sellStock">{{ insufficientQuantity ? 'Insuficiente' : 'Vender' }}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -28,6 +30,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity() {
+            return this.quantity > this.stock.quantity
         }
     },
     methods: {
--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -7,10 +7,12 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
+                <v-text-field label="Quantidade" type="number" 
+                    :error="quantity < 0 || !Number.isInteger(quantity) || insufficientFunds"
+                    v-model.number="quantity"></v-text-field>
                 <v-btn class="green darken-3 white--text" 
-                    :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    @click="buyStock">Comprar</v-btn>
+                    :disabled="quantity <= 0 || !Number.isInteger(quantity) || insufficientFunds"
+                    @click="buyStock">{{ insufficientFunds ? 'Insuficiente' : 'Comprar' }}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -22,6 +24,14 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        },
+        insufficientFunds() {
+            return this.quantity * this.stock.price > this.funds
         }
     },
     methods: {
